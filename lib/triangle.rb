4e2fde87292c97a8d15dfb27ab15a2302348927c
Derek Vogt <derekvogt3@gmail.com>
2022-07-21 13:52:07 -0400

    commit tree 66047582d0a463c0d0d42742a4c2c1636d8e916f

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,56 @@
 class Triangle
-  # write code here
+
+  
+
+
+  def initialize(side_1,side_2,side_3)
+
+    @triangle_array = [side_1,side_2,side_3]
+
+  end
+
+
+
+
+  def kind
+    @triangle_array.each_with_index { |item, index|
+      if item<=0
+        raise TriangleError
+
+      elsif @triangle_array.sum - @triangle_array[index] <= @triangle_array[index]
+
+        raise TriangleError
+
+
+      end
+    }
+
+
+    if @triangle_array[0] == @triangle_array[1] && @triangle_array[1] == @triangle_array[2]
+      return :equilateral
+
+    elsif @triangle_array[0] == @triangle_array[1] || @triangle_array[1] == @triangle_array[2] || @triangle_array[0] == @triangle_array[2] 
+      return :isosceles
+
+    else
+      return :scalene
+
+    end
+
+  end
+
+    class TriangleError  < StandardError
+    end
+
+
+
+
 end
+
+
+
+
+
+    
+
+
